f115b6feb02ebab2d328005e9e40637fea546529
Alvin Unreal <alvin@cmngoal.com>
2025-08-13 19:17:17 +0200

    Use Privacy-First Web Analytics Betterlytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -56,6 +56,14 @@ export default defineNuxtConfig({
     head: {
       charset: "utf-8",
       viewport: "width=device-width, initial-scale=1",
+      script: [
+        {
+          src: "https://betterlytics.io/analytics.js",
+          async: true,
+          "data-site-id": "anxietyaidtools-mea861iq",
+          "data-server-url": "https://betterlytics.io/track"
+        }
+      ]
     },
   },
 
